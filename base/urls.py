2d16b419935d98dbdba94bcf7a91cc70003b5e53
vivek <viveks091827@gmail.com>
2022-01-10 13:56:02 +0530

    ui added

---
+++ base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('user-profile/<str:pk>/', views.user_profile, name='user-profile'),
+
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+        path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
+]
